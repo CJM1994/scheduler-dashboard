990ba49b68e9e4978cb73e8cc8b4cd8ac06b1a55
CJM1994 <connorjohnmullin@gmail.com>
2022-03-31 14:25:25 -0700

    Add display focus state switching

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from './Panel';
 
 import classnames from "classnames";
 
@@ -29,17 +30,31 @@ const data = [
 class Dashboard extends Component {
 
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      'dashboard--focused': this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />
     };
 
-    return <main className={dashboardClasses} />;
+    const panels = data
+      .filter(panel => this.state.focused === null || this.state.focused === panel.id)
+      .map(panel =>
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      );
+
+    return <main className={dashboardClasses}>{panels}</main>;
 
   };
 };
